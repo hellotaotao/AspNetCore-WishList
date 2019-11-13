c1d208cf5db2d34545bc530a99a749998c02925c
Tao Wang <tao.wang@certegy.com.au>
2019-11-13 17:50:38 +1030

    trigger

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,7 +23,7 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); 
             else
                 app.UseExceptionHandler("/Home/Error");
         }
